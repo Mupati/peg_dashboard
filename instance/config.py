457083316447dd7c113513066743d73d860d289d
Mupati <scientificgh@gmail.com>
2019-05-06 10:44:27 +0000

    put db credentials in .env

--- instance/config.py
+++ instance/config.py
@@ -2,4 +2,4 @@ import os
 
 
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-peg-application-key'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://peg_admin:2015ad@localhost/peg_db'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URI')
